b0280f952c1d94a348503356271792c972c4acea
Michaela Bereza <michaelabereza@icloud.com>
2023-09-13 23:44:49 +0300

    local storage

---
+++ src/dummy-profiles.js
@@ -0,0 +1,107 @@
+import MaxAvatar from "./assets/Max_mochi_avatar.jpeg"
+import MichaelaAvatar from "./assets/Michaela_mochi_avatar.jpeg"
+import PukiAvatar from "./assets/Puki_avatar.avif"
+import MukiAvatar from "./assets/Muki_avatar.jpeg"
+
+const DUMMY_PROFILES = [
+  {
+    id: "u1",
+    name: "Max Voronov",
+    profileImage: MaxAvatar,
+    userName: "red",
+    bio: "blablabla",
+    posts: [
+      {
+        id: "p1",
+        caption: "kitty photo"
+      },
+      {
+        id: "p2",
+        caption: "coding"
+      },
+      {
+        id: "p3",
+        caption: "hello world"
+      },
+      {
+        id: "p4",
+        caption: "yes"
+      }
+    ]
+  },
+  {
+    id: "u2",
+    name: "Michaela Voronov",
+    profileImage: MichaelaAvatar,
+    userName: "syntaxcat",
+    bio: "blablabla1111",
+    posts: [
+      {
+        id: "p1",
+        caption: "Chilling & killing"
+      },
+      {
+        id: "p2",
+        caption: "Best day of my life"
+      },
+      {
+        id: "p3",
+        caption: "love my mama <3"
+      },
+      {
+        id: "p4",
+        caption: "another day another..."
+      }
+    ]
+  },
+  {
+    id: "u3",
+    name: "Puki Ben-Puki",
+    profileImage: PukiAvatar,
+    userName: "poopoo",
+    bio: "blablabla4545",
+    posts: [
+      {
+        id: "p1",
+        caption: "bye bye"
+      },
+      {
+        id: "p2",
+        caption: "working hard or hardly working?"
+      },
+      {
+        id: "p3",
+        caption: "you know it"
+      },
+      {
+        id: "p4",
+        caption: "no"
+      }
+    ]
+  },
+  {
+    id: "u4",
+    name: "Muki Ben-Muki",
+    profileImage: MukiAvatar,
+    userName: "muuumooo",
+    bio: "blablabla333",
+    posts: [
+      {
+        id: "p1",
+        caption: "beaching"
+      },
+      {
+        id: "p2",
+        caption: "gyming"
+      },
+      {
+        id: "p3",
+        caption: "dancing"
+      },
+      {
+        id: "p4",
+        caption: "eating"
+      }
+    ]
+  }
+]
